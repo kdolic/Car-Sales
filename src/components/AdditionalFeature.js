2df066bc0f442be465239205c98f6631c8bb51cf
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-01-19 18:03:28 -0600

    MVP

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addFeature} from '../actions/actions';
 
 const AdditionalFeature = props => {
+
+const handleAddFeature = (event) => {
+  event.preventDefault();
+  props.addFeature(props.feature)
+}
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={handleAddFeature}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, {addFeature})(AdditionalFeature)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {reducer} from './reducers/reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
